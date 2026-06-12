a928120df815b7418e22ef002d138d8d10080aac
slm <862860182@qq.com>
2026-06-12 19:00:43 +0800

    fix: Remove non-relevant files

--- dist/types/index.d.ts
+++
@@ -1,5 +0,0 @@
-export * from './types';
-import ParticleCanvas from './ParticleCanvas';
-declare const ParticleCanvasPro: typeof ParticleCanvas;
-export default ParticleCanvasPro;
-export { ParticleCanvas };
